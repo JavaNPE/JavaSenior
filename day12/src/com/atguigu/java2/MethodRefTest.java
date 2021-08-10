af003731bad1c80143a3b32f3dc64c732a826524
dali <daki9981@qq.com>
2021-08-10 15:13:16 +0800

    方法引用的使用

--- day12/src/com/atguigu/java2/MethodRefTest.java
+++ day12/src/com/atguigu/java2/MethodRefTest.java
@@ -44,18 +44,33 @@ public class MethodRefTest {
 		PrintStream ps = System.out;
 		Consumer<String> con2 = ps::println;
 		con2.accept("beijing");
+
+		System.out.println("******************************");
+		Consumer<String> con3 = System.out::println;
+		con3.accept("王大力");
 	}
-	
+
 	//Supplier中的T get()
 	//Employee中的String getName()
 	@Test
 	public void test2() {
 		Employee emp = new Employee(1001,"Tom",23,5600);
 
+		//1.普通(传统)写法
+		Supplier<String> sup3 = new Supplier<String>() {
+			@Override
+			public String get() {
+				return emp.getName();
+			}
+		};
+		System.out.println(sup3.get());
+
+		//2.Lamboda表达式写法
 		Supplier<String> sup1 = () -> emp.getName();
 		System.out.println(sup1.get());
 
 		System.out.println("*******************");
+		//3.方法引用的使用
 		Supplier<String> sup2 = emp::getName;
 		System.out.println(sup2.get());
 
@@ -66,16 +81,29 @@ public class MethodRefTest {
 	//Integer中的int compare(T t1,T t2)
 	@Test
 	public void test3() {
+		//传统写法
+		Comparator<Integer> com3 = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return Integer.compare(o1,o2);
+			}
+		};
+		System.out.println(com3.compare(12,32));
+
+		System.out.println("************************");
+
+		//Lamboda表达式写法
 		Comparator<Integer> com1 = (t1,t2) -> Integer.compare(t1,t2);
 		System.out.println(com1.compare(12,21));
 
 		System.out.println("*******************");
 
+		//方法引用写法
 		Comparator<Integer> com2 = Integer::compare;
 		System.out.println(com2.compare(12,3));
 
 	}
-	
+
 	//Function中的R apply(T t)
 	//Math中的Long round(Double d)
 	@Test
@@ -123,7 +151,7 @@ public class MethodRefTest {
 		BiPredicate<String,String> pre2 = String :: equals;
 		System.out.println(pre2.test("abc","abd"));
 	}
-	
+
 	// Function中的R apply(T t)
 	// Employee中的String getName();
 	@Test
@@ -140,7 +168,15 @@ public class MethodRefTest {
 		Function<Employee,String> func2 = Employee::getName;
 		System.out.println(func2.apply(employee));
 
+		System.out.println("*******************");
 
+		Function<Employee,String> fun3 = new Function<Employee, String>() {
+			@Override
+			public String apply(Employee e) {
+				return e.getName();
+			}
+		};
+		System.out.println(fun3.apply(employee));
 	}
 
 }
